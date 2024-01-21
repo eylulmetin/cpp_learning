dc6f77857d0a8d5174c833236a08dcbf1b47bb3f
Eylul Metin <eylulfmetin@gmail.com>
2024-01-21 20:58:26 +0300

    gitignore updated

---
+++ c_courses/c_courses/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  c_courses
+//
+//  Created by Fehime Eylül Metin on 21.01.2024.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
