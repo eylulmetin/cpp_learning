6241f4037133237c94a2d3671638c4138074aa7a
Eylul Metin <eylulfmetin@gmail.com>
2024-01-21 21:04:13 +0300

    cpp src file added

---
+++ cpp_courses/src/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  cpp_courses
+//
+//  Created by Fehime Eylül Metin on 21.01.2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
